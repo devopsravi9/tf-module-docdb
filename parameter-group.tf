97bc95efd5ff6b7fad886eb5e2be48f70bc35519
Ravi <ravi@gmail.com>
2022-08-01 07:59:47 +0530

    ec2 code

--- parameter-group.tf
+++ parameter-group.tf
@@ -1,5 +1,5 @@
 resource "aws_docdb_cluster_parameter_group" "main" {
-  family      = var.PG_FAMILY
+  family      = var.DOCDB_PG_FAMILY
   name        = "${local.TAG_PREFIX}-pg"
   description = "${local.TAG_PREFIX}-parameter-group"
 }
